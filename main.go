576548d218f1382b5d471a01e9d90d70ef592a4e
gaofeng <57412960@qq.com>
2025-04-08 15:04:14 +0800

    初始化数据库

--- main.go
+++ main.go
@@ -18,12 +18,15 @@ func main() {
 
 	// 配置初始化，依赖命令行 --env 参数
 	var env string
-	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .evn.testing 文件")
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
 	flag.Parse()
 	config.InitConfig(env)
 
 	// new 一个 Gin Engine 实例
 	router := gin.New()
+
+	// 初始化 DB
+	bootstrap.SetupDB()
 
 	// 初始化路由绑定
 	bootstrap.SetupRoute(router)
